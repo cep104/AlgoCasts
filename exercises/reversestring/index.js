f290d16c9ca245ad09d508121e1f6697db8ee733
cep104 <candicelizabethpeters@gmail.com>
2020-12-03 22:30:01 -0600

    finished

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -5,13 +5,29 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
+// solution 1:
+// function reverse(str) {
+//   return str.split("").reverse().join("")
+   
+// }
 
-function reverse(str) {
-   let array = str.split("")
-   array.reverse()
-   let stringReversed = array.join("")
-   return stringReversed
+// function reverse(str){
+//     let reversed = '';
+//     for(letter of str){
+//         reversed = letter + reversed;
+//     }
+//     return reversed;
+// }
+
+function reverse(str){
+  return  str.split('').reduce((reversed, character)=>{
+        return character + reversed;
+        // take the character we are opperating on right now 
+        //add it to the total reversed string and return the result
+    }, '');
+
 }
+// using reduce to reverse the string is the best way to show the interviewer you know what you are doing
 
 module.exports = reverse;
 // #finished
