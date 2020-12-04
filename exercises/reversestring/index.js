6b779e5e188a02f753232853cced00c2d9bcf69c
cep104 <candicelizabethpeters@gmail.com>
2020-12-03 23:03:36 -0600

    recursion

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -5,6 +5,9 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
+
+
+
 // solution 1:
 // function reverse(str) {
 //   return str.split("").reverse().join("")
@@ -19,15 +22,33 @@
 //     return reversed;
 // }
 
-function reverse(str){
-  return  str.split('').reduce((reversed, character)=>{
-        return character + reversed;
-        // take the character we are opperating on right now 
-        //add it to the total reversed string and return the result
-    }, '');
+// function reverse(str){
+//   return  str.split('').reduce((reversed, character)=>{
+//         return character + reversed;
+//         // take the character we are opperating on right now 
+//         //add it to the total reversed string and return the result
+//     }, '');
 
-}
+// }
 // using reduce to reverse the string is the best way to show the interviewer you know what you are doing
+
+//using recursion 
+//runs through the string through the function till the string is done("")
+//once it is done return the new string 
+//making function 
+function reverse(str){
+    if(str === ""){
+        
+     return str 
+    }else{
+        
+     return reverse(str.substr(1)) + str[0]
+    }
+   }
+   //substr cuts off the string at the index you put
+   //first time return bcd + a (bcda)
+   //second time return cd + b (cdba)
+   //third time return d  (dcba)
 
 module.exports = reverse;
 // #finished
